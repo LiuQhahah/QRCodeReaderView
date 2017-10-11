a39e27e46ca673bb8bc8d715e3da8f975b2781a0
LiuQhahah <liuq_1995@163.com>
2017-10-11 20:57:26 +0800

    将SmartChart移植到本项目中，用MVVM架构
    (cherry picked from commit 4a1b40c)

---
+++ samples/src/main/java/com/example/qr_readerexample/activity/DataTrendActivity.java
@@ -0,0 +1,15 @@
+package com.example.qr_readerexample.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qr_readerexample.R;
+
+public class DataTrendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_trend);
+    }
+}
